a770e0e1c6e2ea472ece4c9a13406fd46d8a5720
Austin Cathey <thepeoplescoder@gmail.com>
2025-11-23 01:49:52 -0800

    Adding an alias for gls if it exists.

--- include/aliases/for_color.sh
+++ include/aliases/for_color.sh
@@ -8,3 +8,5 @@ alias ls="$(
 alias grep='grep --color=auto '
 alias ip='ip -color=auto '
 alias diff='diff --color=auto '
+
+command_exists 'gls' && alias gls="LC_COLLATE=C gls --group-directories-first --color=auto"
