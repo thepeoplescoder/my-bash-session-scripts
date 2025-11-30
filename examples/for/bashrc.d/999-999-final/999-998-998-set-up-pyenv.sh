f86b95a779e9f91247fc1fac261ac983e3977731
Austin Cathey <thepeoplescoder@gmail.com>
2025-11-29 23:59:02 -0800

    Added an example file for setting up pyenv.

---
+++ examples/for/bashrc.d/999-999-final/999-998-998-set-up-pyenv.sh
@@ -0,0 +1,17 @@
+PYENV_ROOT="$HOME/.pyenv"
+
+if command -v pyenv 1>/dev/null 2>&1; then
+    PYENV_ROOT=''
+elif is_a_directory "$PYENV_ROOT"; then
+    prepend_to_PATH_if_it_exists "$PYENV_ROOT/bin"
+fi
+
+if ! command_exists 'pyenv'; then
+    unset PYENV_ROOT
+    return 0
+fi
+
+eval "$(pyenv init - bash)"
+
+[[ ! "$PYENV_ROOT" ]] && PYENV_ROOT="$(pyenv root)"
+export PYENV_ROOT
