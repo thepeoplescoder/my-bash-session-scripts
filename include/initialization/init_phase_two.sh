22d2913319aa465caeba313cee8b9b727222f652
Austin Cathey <thepeoplescoder@gmail.com>
2025-11-30 04:47:46 -0800

    Initialization process now checks for existence of CYGWIN or MSYS2 in the appropriate environments on Windows.

--- include/initialization/init_phase_two.sh
+++ include/initialization/init_phase_two.sh
@@ -1,26 +1,26 @@
-if [[ -v __OS__ && -v __OS_IS_WINDOWS__ ]]; then
-    return 0
+if [[ ! -v __OS__ ]]; then
+    export __OS__="$(
+        case "$(uname -s)" in
+            Linux)          echo "linux"     ;;
+            *BSD|DragonFly) echo "bsd"       ;;
+            Darwin)         echo "macos"     ;;
+            CYGWIN*)        echo "cygwin"    ;;
+            MSYS*)          echo "msys2"     ;;
+            MINGW32*)       echo "mingw32"   ;;
+            MINGW64*)       echo "mingw64"   ;;
+            *)              echo "<unknown>" ;;
+        esac
+    )"
 fi
 
-export __OS__="$(
-	case "$(uname -s)" in
-		Linux)          echo "linux"     ;;
-		*BSD|DragonFly) echo "bsd"       ;;
-		Darwin)         echo "macos"     ;;
-		CYGWIN*)        echo "cygwin"    ;;
-		MSYS*)          echo "msys2"     ;;
-		MINGW32*)       echo "mingw32"   ;;
-		MINGW64*)       echo "mingw64"   ;;
-		*)              echo "<unknown>" ;;
-	esac
-)"
-
-export __OS_IS_WINDOWS__="$(
-	case "$__OS__" in
-		cygwin|msys2|mingw32|mingw64) echo "true"  ;;
-		*)                            echo "false" ;;
-	esac
-)"
+if [[ ! -v __OS_IS_WINDOWS__ ]]; then
+    export __OS_IS_WINDOWS__="$(
+        case "$__OS__" in
+            cygwin|msys2|mingw32|mingw64) echo "true"  ;;
+            *)                            echo "false" ;;
+        esac
+    )"
+fi
 
 function set_CYGWIN_or_MSYS2_for_windows_operating_systems() {
     unset -f "$FUNCNAME"
@@ -37,15 +37,22 @@ function set_CYGWIN_or_MSYS2_for_windows_operating_systems() {
         return 1
     fi
 
-    echo -n "Executing: "
     case "$__OS__" in
-        msys2|mingw32|mingw64) 
-            echo "export MSYS2=\"$text\""
-            export MSYS2="$text"
+        msys2|mingw32|mingw64)
+            if [ -v MSYS2 ]; then
+                echo "MSYS2 already set to $MSYS2"
+            else
+                echo "Executing: export MSYS2=\"$text\""
+                export MSYS2="$text"
+            fi
             ;;
         cygwin)
-            echo "export CYGWIN=\"$text\""
-            export CYGWIN="$text"
+            if [ -v CYGWIN ]; then
+                echo "CYGWIN already set to $CYGWIN"
+            else
+                echo "Executing: export CYGWIN=\"$text\""
+                export CYGWIN="$text"
+            fi
             ;;
     esac
 }
