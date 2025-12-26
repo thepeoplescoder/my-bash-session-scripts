83dd60cc6a87b0796733687b7ce31f487737b083
Austin Cathey <thepeoplescoder@gmail.com>
2025-12-26 05:17:33 -0800

    Moved command_exists to include/initialization/initial_utility_functions.sh.  The example file bashrc.d/001-000-initial/001-000-000-load-colorful-aliases.sh now has a function that intercepts the alias shell builtin, adding color to the known aliases if no arguments are passed.

--- examples/for/bashrc.d/001-000-initial/001-000-000-load-colorful-aliases.sh
+++ examples/for/bashrc.d/001-000-initial/001-000-000-load-colorful-aliases.sh
@@ -1,1 +1,26 @@
 source "$__MY_INCLUDE_DIR__/aliases/_.sh"
+
+! command_exists 'sed' && return 0
+
+function_template=$(
+cat <<'SHELL'
+function alias() {
+    if [ $# -lt 1 ]; then
+        local pattern='^\(alias \)\([^=]*\)=\(.*\)$'
+        local replace="\\1{__COLOR1__}\\2{__RESET__}={__COLOR2__}\\3{__RESET__}"
+        builtin alias | sed -e "s/$pattern/$replace/"
+    else
+        builtin alias "$@"
+    fi
+}
+SHELL
+)
+
+function_body=$function_template
+function_body=${function_body//'{__RESET__}'/"$(__ansi__ reset)"}
+function_body=${function_body//'{__COLOR1__}'/'\x1b[1m'"$(__ansi__ bright green)"}
+function_body=${function_body//'{__COLOR2__}'/"$(__ansi__ color cyan)"}
+
+eval "$function_body"
+
+unset function_template function_body
--- include/initialization/initial_utility_functions.sh
+++ include/initialization/initial_utility_functions.sh
@@ -4,10 +4,12 @@ fi
 
 __INITIALIZATION_SLASH_INITIAL_UTILITY_FUNCTIONS_DOT_SH__="$(get_this_file_name)"
 
+unset_on_exit is_top_level_or_sourced
 function is_top_level_or_sourced() {
 	[[ ! "${FUNCNAME[2]:-}" || "${FUNCNAME[2]}" == "source" ]]
 }
 
+unset_on_exit expect
 function expect() {
     local value="$1"
     local dummyIs="$2"
@@ -16,4 +18,10 @@ function expect() {
     [[ "$dummyIs" == "--is"           ]] || abort "${FUNCNAME[0]}: --is expected."
     [[ "$value"   == "$expectedValue" ]] || abort "${FUNCNAME[1]} $expectedValue expected."
 }
+
+unset_on_exit command_exists
+function command_exists() {
+    command -v "$@" &> /dev/null
+}
+
 
